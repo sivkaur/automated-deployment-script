0ce0b0b1ba6b4dac697331eabad2de67a455e729
A00474480 <146491766+sivkaur@users.noreply.github.com>
2024-06-17 12:34:17 -0300

    updated the port
    updated the port

--- polling_script.py
+++ polling_script.py
@@ -4,7 +4,7 @@ from mlflow.tracking import MlflowClient
 import boto3
 
 # Set the MLflow tracking URI to the localhost server
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("http://127.0.0.1:5001")
 
 # Initialize MLflow client
 client = MlflowClient()
